f67364622ff1c6772e23fbfc6a09bd912d58aff9
zombiehunter <viktormallory25@gmail.com>
2024-01-16 16:53:57 +0300

    HW5 task02 solved

---
+++ HomeWork_5/task02/NamesCounter.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+
+class NamesCounter {
+    private HashMap<String, Integer> names = new HashMap<>();
+
+    // Метод для добавления имени в HashMap
+    public void addName(String name) {
+        // Введите свое решение ниже
+        var value = names.putIfAbsent(name, 1);
+        if (value != null) {
+            names.merge(name, 1, Integer::sum);
+        }
+    }
+
+    // Метод для вывода содержимого HashMap
+    public void showNamesOccurrences() {
+        // Введите свое решение ниже
+        System.out.println(names);
+    }
+}
